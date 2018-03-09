16f402e37a16a850d77877b5808cb5e536d57c3a
emre <test@example.com>
2018-03-09 00:41:26 -0600

    Protect requests and implement auth guard

--- app.js
+++ app.js
@@ -37,8 +37,12 @@ app.use('/users', users);
 
 app.get('/', (req, res) => {
     res.send('it is working!!');
-})
+});
+
+app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
-})
+});
